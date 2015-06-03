c562983f189f73b7b4991e8e8e832fcc8838991a
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-06-03 11:12:36 +0200

    Get more information inter the image processing

--- Classes/Service/ImageService.php
+++ Classes/Service/ImageService.php
@@ -20,6 +20,22 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class ImageService extends \TYPO3\CMS\Extbase\Service\ImageService {
 
 	/**
+	 * @var null
+	 */
+	protected $imageForRealName = NULL;
+
+	/**
+	 * @param \TYPO3\CMS\Core\Resource\File|\TYPO3\CMS\Core\Resource\FileReference $image
+	 * @param array                                                                $processingInstructions
+	 *
+	 * @return ProcessedFile
+	 */
+	public function applyProcessingInstructions($image, $processingInstructions) {
+		$this->imageForRealName = $image;
+		return parent::applyProcessingInstructions($image, $processingInstructions);
+	}
+
+	/**
 	 * Get public url of image depending on the environment
 	 *
 	 * @param FileInterface $image
@@ -36,8 +52,9 @@ class ImageService extends \TYPO3\CMS\Extbase\Service\ImageService {
 		$tx_flrealurlimage->start(NULL, NULL);
 		if ($image instanceof ProcessedFile) {
 			$info = array(3 => $imageUrl);
-			$imageUrl = $tx_flrealurlimage->main(array(), $info, $image->getOriginalFile());
+			$imageUrl = $tx_flrealurlimage->main(array(), $info, $this->imageForRealName);
 		}
+		$this->imageForRealName = NULL;
 
 		// no prefix in case of an already fully qualified URL (having a schema)
 		if (strpos($imageUrl, '://')) {
