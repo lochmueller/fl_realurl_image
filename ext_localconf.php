de2cedf69e665d6e35ae32644028cd964a958e97
Dennis Geldmacher <geldmacher.dennis@googlemail.com>
2015-06-17 17:55:56 +0200

    [FEATURE] XClass for Uri/ImageViewHelper
    I think this is missing! =)

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,6 +11,7 @@ $xClass = array(
 	'TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer'      => 'ContentObjectRenderer',
 	'TYPO3\\CMS\\Frontend\\ContentObject\\ImageResourceContentObject' => 'ImageResource',
 	'TYPO3\\CMS\\Fluid\\ViewHelpers\\ImageViewHelper'                 => 'ImageViewHelper',
+	'TYPO3\\CMS\\Fluid\\ViewHelpers\\Uri\\ImageViewHelper'            => 'UriImageViewHelper',
 );
 foreach ($xClass as $source => $target) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][$source] = array('className' => 'FRUIT\\FlRealurlImage\\Xclass\\' . $target);
