5e92460bef53bdb0971dc66aed1cf764ca38e69c
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-06-14 15:45:06 +0200

    [TASK] Add first Unit test

---
+++ Tests/Unit/Cache/UriFrontendTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * UriFrontendTest
+ */
+
+namespace FRUIT\FlRealurlImage\Tests\Unit\Cache;
+
+use FRUIT\FlRealurlImage\Cache\UriFrontend;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * UriFrontendTest
+ */
+class UriFrontendTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function cacheIdentifierIsAlwaysValid()
+    {
+        $cacheFrontend = new UriFrontend();
+        $this->assertEquals(true, $cacheFrontend->isValidEntryIdentifier('Strange code here'));
+    }
+}
